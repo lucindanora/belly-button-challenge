178f4545520e4a4e39b9a91732087f20e4ea666e
lucindanora <144958544+lucindanora@users.noreply.github.com>
2024-01-13 08:44:42 -0600

    Add files via upload

---
+++ Module_14_Starter_Code/StarterCode/static/js/app.js
@@ -0,0 +1,21 @@
+// get json files
+
+const url = https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json;
+
+// Fetch the JSON data and console log it
+
+d3.json(url).then(function(data) {
+    console.log(data);
+  });
+  
+  //create a hosizontal bar chat with dropdown menu to display top 10 OTUs found in that individual 
+
+  //create a bubble chart that diplays each sample
+
+  //display sample metadata, ie an individuals demo info
+
+  //display each key-value pair from the meadata JSON object somewhere on the page
+
+  //update all the plots when a new sample is selected.
+
+  
